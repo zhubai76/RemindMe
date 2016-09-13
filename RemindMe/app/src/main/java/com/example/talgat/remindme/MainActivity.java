de8e6596a4bf713fc2685b2416a225ede5bd4bc9
zhubai76 <zhubai76@gmail.com>
2016-09-13 23:11:15 +0500

    Create MainActivity and  layot

---
+++ RemindMe/app/src/main/java/com/example/talgat/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.talgat.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Talgat on 13.09.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
