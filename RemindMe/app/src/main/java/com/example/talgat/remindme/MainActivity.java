3fda7e3843b5774bd6c3c966818d7f1ff07afb8f
zhubai76 <zhubai76@gmail.com>
2016-09-23 12:23:36 +0500

    toolbar-drawable

--- RemindMe/app/src/main/java/com/example/talgat/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/example/talgat/remindme/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.talgat.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by Talgat on 13.09.2016.
- */
+
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
     }
+
+    private void initToolbar(){
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener()){
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem){
+                return false;
+
+            }
+        }    };
+    toolbar.inflatemenu(R.menu.menu);
 }
